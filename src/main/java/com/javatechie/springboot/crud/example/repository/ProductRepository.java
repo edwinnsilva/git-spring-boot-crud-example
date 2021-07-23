6e5e23167e730ce5be4b70afb035de67804fdc2e
edwinn silva <edwinn.silvalamus@gmail.com>
2021-07-23 02:02:57 -0500

    mi primer commit

---
+++ src/main/java/com/javatechie/springboot/crud/example/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.javatechie.springboot.crud.example.repository;
+
+import com.javatechie.springboot.crud.example.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+     Product findByname(String name);
+    }
